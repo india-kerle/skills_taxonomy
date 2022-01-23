bf8a044bf5d31aaeae92957e26ce8839b58cacfa
India Kerle <india.kerle@nesta.org.uk>
2022-01-23 14:45:01 +0000

    added pipeline .py files

---
+++ src/build_network.py
@@ -0,0 +1,107 @@
+"""
+Builds a weighted, undirected network of essential skill pairs 
+from occupations data. Saves network locally as pickle file. 
+
+In activated conda environment, run python build_network.py 
+"""
+#####################################################################
+
+import itertools
+from collections import Counter
+import networkx as nx
+import pandas as pd 
+
+from utils.utils import ( 
+    PROJECT_DIR, 
+    CONFIG_PATH, 
+    DATA_OUTPUTS_PATH,
+    get_yaml_config,
+    save_data,
+    load_data)
+
+#####################################################################
+
+def build_skills_network(occupations_data, edgeweight_prune_threshold):
+    
+    occupations = list(occupations_data.keys())
+    all_pairs = []
+
+    for occupation in occupations:
+        job = occupations_data[occupation]
+        if "hasEssentialSkill" in job['_links'].keys(): 
+            required_skills = [x['title'] for x in job['_links']['hasEssentialSkill']]
+            sorted(required_skills)
+            pairs = list(itertools.combinations(required_skills,2))
+            all_pairs.append(pairs)
+        else:
+            print("skills that have no hasEssentialSkill: ", occupation)
+
+    all_pairs = Counter(itertools.chain(*all_pairs)).most_common()
+    
+    #create dataframe for network
+
+    skill_pairs = [(record[0][0], record[0][1], record[1]) for record in all_pairs]
+    skill_df = pd.DataFrame(skill_pairs, columns = ['node1','node2','weight']) 
+    
+    #deal with directionality issues - add the weights together
+    df2 = skill_df.copy()
+    node1list = skill_df['node1']
+    node2list = skill_df['node2']
+    df2['node1'] = node2list
+    df2['node2'] = node1list
+
+    df3 = df2.append(skill_df)
+    df4 = df3.groupby(['node1','node2']).sum().reset_index()
+    
+    #skills that cooccur in at least n job titles 
+    df5 = df4[df4['weight'] > edgeweight_prune_threshold]
+
+    #create weighted network with data
+    G = nx.from_pandas_edgelist(df5, source= 'node1',
+                                target = 'node2', 
+                                edge_attr = 'weight')
+    
+    return G
+
+def prune_skills_network(skills_network, bad_coefs):
+    
+    clustering_coefs = nx.clustering(skills_network)
+
+    #subset clusterng coefs based on being v low i.e. highly transversal skills 
+    nodes = list(clustering_coefs.keys())
+    #graph clustering coef values to choose bad_coef
+    pd.DataFrame(list(clustering_coefs.values())).plot.hist()
+
+    clustering_coefs_to_get_rid_of = []
+    for node in nodes:
+        if clustering_coefs[node] < bad_coefs: #based on plot
+            clustering_coefs_to_get_rid_of.append({node: clustering_coefs[node]})
+
+    clustering_coefs_to_get_rid_of = {k:v for el in clustering_coefs_to_get_rid_of for k,v in el.items()}
+    print(f'removing {len(clustering_coefs_to_get_rid_of)} nodes based on clustering coef')
+    
+    #remove skill nodes based on clustering coef
+    bad_skills = list(clustering_coefs_to_get_rid_of.keys())
+    skills_network.remove_nodes_from(bad_skills)
+    
+    return skills_network
+
+if __name__ == "__main__":
+
+    #get config file with relevant paramenters
+    config_info = get_yaml_config(CONFIG_PATH)
+    #path for occupation data
+    data_path = config_info['occupations_data_path']
+    #threshold for edge weight
+    edgeweight_threshold = config_info['edgeweight_prunethreshold']
+    #bad coefs based on graph
+    bad_coefs = config_info["bad_coefs"]
+    #graph file name
+    skills_graph_name = config_info["skills_graph_name"]
+
+    occupations_data = load_data(str(PROJECT_DIR) + data_path)
+    skills_network = build_skills_network(occupations_data, edgeweight_threshold)
+    pruned_skills_network = prune_skills_network(skills_network, bad_coefs)
+
+    #save pruned graph locally
+    save_data(pruned_skills_network, str(DATA_OUTPUTS_PATH) + skills_graph_name)
---
+++ src/cluster_network.py
@@ -0,0 +1,126 @@
+"""
+Clusters and labels clusters of a weighted, undirected network of 
+essential skill pairs from occupations data.
+
+Saves clustered network locally as pickle file.  
+
+In activated conda environment, run python cluster_network.py 
+"""
+#####################################################################
+
+from community import community_louvain
+from collections import Counter
+import itertools
+import networkx as nx
+
+from utils.utils import ( 
+    DATA_OUTPUTS_PATH,
+    CONFIG_PATH,
+    get_yaml_config,
+    preprocess_skills,
+    load_data,
+    save_data)
+
+#####################################################################
+
+def detect_skills_communities(G):
+
+    #run community detection algorithm on network
+
+    dendo = community_louvain.generate_dendrogram(G,random_state=42,resolution=1)
+
+    dendo_cluster_dict ={}
+    all_skills = list(G.nodes())
+
+    for skill in all_skills:
+        cluster_id = ''
+        for level in range(len(dendo)):
+            cluster_id_at_level = community_louvain.partition_at_level(dendo, level)[skill]
+            cluster_id = cluster_id + '-' + str(cluster_id_at_level)
+        
+        #flip hierarchy to largest cluster first  
+        hierarchy = cluster_id[1:]
+        reverse_hierarchy = "-".join(hierarchy.split('-')[::-1])
+        
+        dendo_cluster_dict[skill] = {'cluster_group': reverse_hierarchy,
+                                     'cluster_subgroup0': reverse_hierarchy.split('-')[0],
+                                     'cluster_subgroup1': reverse_hierarchy.split('-')[1],
+                                     'cluster_subgroup2': reverse_hierarchy.split('-')[2]
+            
+        }
+
+    for node, node_attributes in dendo_cluster_dict.items():
+        G.nodes[node]['cluster_group'] = node_attributes['cluster_group']
+        G.nodes[node]['cluster_subgroup0'] = node_attributes['cluster_subgroup0']
+        G.nodes[node]['cluster_subgroup1'] = node_attributes['cluster_subgroup1']
+        G.nodes[node]['cluster_subgroup2'] = node_attributes['cluster_subgroup2']
+
+    #returns both network and node attributes per node
+    return G, {key for nodes in dendo_cluster_dict.values() for key in nodes}
+
+def label_skills_communities(G, cluster_granularity, top_n):
+    """labels all skills communities per cluster granularity."""
+    
+    unique_cluster_ids = list(set([node[1] for node in G.nodes(data=cluster_granularity)]))
+    cluster_names = {}
+
+    for cluster_id in unique_cluster_ids:
+        cluster_words = [node[0] for node in G.nodes(data=cluster_granularity) if node[1] == cluster_id]
+        token_list = preprocess_skills(cluster_words)
+        flat_list = list(itertools.chain(*token_list))
+        word_counts = Counter(flat_list).most_common()
+        top_n_words = [x[0] for x in word_counts[0:top_n]]
+        cluster_description = " | ".join(top_n_words)
+        cluster_names[cluster_id] = cluster_description
+    
+    node_cluster_names = dict(zip(list(G.nodes()),
+                                  [cluster_names[G.nodes[n][cluster_granularity]] for n in G.nodes]))
+    
+    nx.set_node_attributes(G, node_cluster_names, cluster_granularity + '_name')
+
+    return G
+    
+def evaluate_skills_communities(G, cluster_granularity):
+
+    #calculate overall modularity at the highest level
+    partition = dict(G.nodes(data=cluster_granularity))
+    
+    modularity = community_louvain.modularity(partition, G, weight='weight')
+    print('overall network modularity: ', round(modularity,2))
+
+    #calculate modularity per for subgroup and print modularity if modularity not 0
+    unique_clusters = list(set(list(dict(G.nodes(data='cluster_subgroup0')).values())))
+
+    for unique_cluster in unique_clusters:
+        nodes = {node: data['cluster_group'] for node, data in G.nodes(data=True) if data.get('cluster_subgroup0') == unique_cluster}
+        G_subgroup = G.subgraph(list(nodes.keys())) 
+        modularity = community_louvain.modularity(nodes, G_subgroup, weight='weight')
+        if modularity != 0:
+            print("cluster id: ", unique_cluster, "modularity: ", round(modularity,2))
+    
+
+if __name__ == "__main__":
+
+    #get config file with relevant paramenters
+    config_info = get_yaml_config(CONFIG_PATH)
+
+    skills_graph_path = config_info["skills_graph_name"]
+    top_n = config_info['top_cluster_top_names']
+    clustered_skills_graph_path = config_info['clustered_skills_graph_name']
+    top_cluster_granularity = config_info['cluster_granularity']
+
+    #load network data
+    G_skills = load_data(str(DATA_OUTPUTS_PATH) + skills_graph_path)
+
+    #hierarchically cluster skills network
+    G_skills_clustered, cluster_granularities = detect_skills_communities(G_skills)
+
+    #add cluster labels per cluster granularity 
+    for cluster_granularity in cluster_granularities:
+        label_skills_communities(G_skills_clustered, cluster_granularity, top_n)
+
+    #evaluate skill communities
+    evaluate_skills_communities(G_skills_clustered, top_cluster_granularity)
+
+    #save clustered network locally
+    save_data(G_skills_clustered, str(DATA_OUTPUTS_PATH) + clustered_skills_graph_path)
